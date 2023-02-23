d3e8d4850cb43f6472164ad2b143c1e3229d7309
Blake Creasser <blakecreasser@aggienetwork.com>
2023-02-23 00:15:30 -0600

    commit tree 9a88b50340c1bfb71738b497b6a4e250b4d38295

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,43 @@
+require 'pry'
 
+class CashRegister
+
+    attr_accessor :total, :items, :last_item
+    attr_reader :discount
+
+    def initialize(discount = 0)
+        @discount = discount
+        @total = 0
+        @items = []
+    end
+
+    #def total
+    #    @total
+    #end
+
+    def add_item (title, price, quantity = 1)
+        counter = quantity
+        until counter <= 0
+            @items << title 
+            counter -= 1
+        end
+        self.last_item = price * quantity
+        self.total += self.last_item
+        
+    end
+
+    def apply_discount
+        if self.discount > 0
+            @total = @total - (@total.to_f * (@discount / 100.0))
+            "After the discount, the total comes to $#{@total.to_i}."
+        else
+            'There is no discount to apply.'
+        end
+    end
+
+    def void_last_transaction()
+        self.total -= self.last_item
+    end
+
+
+end
